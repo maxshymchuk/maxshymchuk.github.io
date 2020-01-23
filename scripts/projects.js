765b2048a653839496c6c0b040baf4be198cc33c
Max Shymchuk <octorix@mail.ru>
2020-01-24 01:34:30 +0300

    feat: add scripts & restruct

---
+++ scripts/projects.js
@@ -0,0 +1,122 @@
+const ProjectType = {
+  repo: 'repo',
+  website: 'website'
+}
+
+export const projects = [
+  {
+    name: 'Font Viewer',
+    link: 'https://github.com/maxshymchuk/Font-Viewer',
+    type: ProjectType.repo
+  },
+  {
+    name: 'Mifort Adaptive',
+    link: 'https://maxshymchuk.github.io/Mifort-2',
+    type: ProjectType.website
+  },
+  {
+    name: 'JSPhysics',
+    link: 'https://maxshymchuk.github.io/JSPhysics',
+    type: ProjectType.website
+  },
+  {
+    name: 'RSSchool Layout',
+    link: 'https://maxshymchuk.github.io/RSSchool-theyalow',
+    type: ProjectType.website
+  },
+  {
+    name: 'Tudushka',
+    link: 'http://maxshymchuk.github.io/Tudushka',
+    type: ProjectType.website
+  },
+  {
+    name: 'Vault',
+    link: 'https://github.com/maxshymchuk/Vault',
+    type: ProjectType.repo
+  },
+  {
+    name: 'VaultPC',
+    link: 'https://github.com/maxshymchuk/VaultPC',
+    type: ProjectType.repo
+  },
+  {
+    name: 'S-S Cookie',
+    link: 'https://github.com/maxshymchuk/SaM-Solutions-5',
+    type: ProjectType.repo
+  },
+  {
+    name: 'Mifort CSV Parser',
+    link: 'https://github.com/maxshymchuk/Mifort-4',
+    type: ProjectType.repo
+  },
+  {
+    name: 'EasyNote',
+    link: 'https://github.com/maxshymchuk/EasyNote',
+    type: ProjectType.repo
+  },
+  {
+    name: 'Sudoku',
+    link: 'https://maxshymchuk.github.io/Sudoku',
+    type: ProjectType.website
+  },
+  {
+    name: 'S-S Review',
+    link: 'http://maxshymchuk.github.io/SaM-Solutions-3',
+    type: ProjectType.website
+  },
+  {
+    name: 'Game of Life',
+    link: 'https://github.com/maxshymchuk/Game-of-Life',
+    type: ProjectType.repo
+  },
+  {
+    name: 'oaip.github.io',
+    link: 'https://oaip.github.io/',
+    type: ProjectType.website
+  },
+  {
+    name: 'S-S Layout',
+    link: 'http://maxshymchuk.github.io/SaM-Solutions-2',
+    type: ProjectType.website
+  },
+  {
+    name: 'RSSchool CV',
+    link: 'https://maxshymchuk.github.io/rsschool-cv/',
+    type: ProjectType.website
+  },
+  {
+    name: 'S-S Course Light Project',
+    link: 'https://github.com/maxshymchuk/food-restaurant',
+    type: ProjectType.repo
+  },
+  {
+    name: 'RSOA',
+    link: 'https://github.com/maxshymchuk/RSOA',
+    type: ProjectType.repo
+  },
+  'Mifort Dropdown',{
+    name: 'Mifort Dropdown',
+    link: 'https://github.com/maxshymchuk/Mifort-1',
+    type: ProjectType.repo
+  },
+  {
+    name: 'Cryptography',
+    link: 'http://maxshymchuk.github.io/Cryptography',
+    type: ProjectType.website
+  },
+  {
+    name: 'UnoRetranslator',
+    link: 'https://github.com/maxshymchuk/UnoRetranslator',
+    type: ProjectType.repo
+  },
+  {
+    name: 'S-S GitHub Repos Stats',
+    link: 'http://maxshymchuk.github.io/SaM-Solutions-4',
+    type: ProjectType.website
+  },
+  {
+    name: 'Mifort Layout',
+    link: 'https://maxshymchuk.github.io/Mifort-3',
+    type: ProjectType.website
+  }
+]
---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+import { projects } from './projects.js';
+
+function shuffle(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const pos = Math.round(Math.random() * (arr.length - 1));
+    [arr[i], arr[pos]] = [arr[pos], arr[i]];
+  }
+  return arr;
+}
+
+document.body.onload = () => {
+  particlesJS.load('particles-js', 'scripts/particles.json');
+  const shuffled = shuffle(projects);
+  const templateProjectElem = document.getElementById('template__project');
+  const projectList = document.getElementById('projects');
+  shuffled.forEach(i => {
+    const projectElem = templateProjectElem.content.cloneNode(true).querySelector('a');
+    projectElem.classList.add(i.type);
+    projectElem.setAttribute('href', i.link);
+    projectElem.innerText = i.name;
+    projectList.appendChild(projectElem);
+  })
+}
