d6a98a2feb772960510ee5c6d50bd0c824efc325
Max Shymchuk <octorix@mail.ru>
2024-05-29 15:20:35 +0200

    fix (service): add git pull before push

--- service/src/modules/git.ts
+++ service/src/modules/git.ts
@@ -5,6 +5,7 @@ const git: SimpleGit = simpleGit();
 export default async function commitAndPush(message: string): Promise<void> {
     if (!process.env.USER || !process.env.TOKEN || !process.env.REPO) return;
     git
+        .pull('origin', 'master')
         .add(globalThis.dataPath)
         .commit(message)
         .push(`https://${process.env.USER}:${process.env.TOKEN}@github.com/${process.env.USER}/${process.env.REPO}`, 'master');
