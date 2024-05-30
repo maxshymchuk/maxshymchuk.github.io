50a46851a19614cee388cc587153cbce239dfbfa
Max Shymchuk <octorix@mail.ru>
2024-05-31 01:09:58 +0200

    test: update public token

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
 namespace Constants {
-    export const publicKey = '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';
+    export const publicKey = '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';
     export const fileName = 'data.json';
 }
 
