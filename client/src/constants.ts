730f4c3ccb00ba461805a37cf9bd8c23c06383d5
Max Shymchuk <octorix@mail.ru>
2024-06-06 14:22:27 +0200

    fix (client): update vercel url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -3,7 +3,7 @@ namespace Constants {
 }
 
 namespace Sources {
-    export const vercelUrl = 'https://maxshymchuk-github-io-maxshymchuk-maxshymchuks-projects.vercel.app/';
+    export const vercelUrl = 'https://maxshymchuk-github-io.vercel.app/';
     export const gistUrl = 'https://api.github.com/gists/ec571ee4e7aa22601cf20c8084786924';
 }
 
