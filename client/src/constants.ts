1dccdf867b2067d54c1fd9c4e497228b77412b35
Max Shymchuk <octorix@mail.ru>
2024-06-11 16:36:41 +0200

    fix (client): update gist source

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,10 +1,10 @@
 namespace Constants {
-    export const fileName = 'data.json';
+    export const fileName = 'maxshymchuk-backup.json';
 }
 
 namespace Sources {
     export const vercelUrl = 'https://maxshymchuk-github-io.vercel.app';
-    export const gistUrl = 'https://api.github.com/gists/ec571ee4e7aa22601cf20c8084786924';
+    export const gistUrl = 'https://api.github.com/gists/84b49b11e68e3ddc6a8ef0491bb477c9';
 }
 
 export { Constants, Sources };
