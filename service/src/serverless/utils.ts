d59a2f00947e131f1eea334470cebbbc785a2c8f
Max Shymchuk <octorix@mail.ru>
2024-06-11 19:10:40 +0200

    fix (service): function handler

--- service/src/serverless/utils.ts
+++ service/src/serverless/utils.ts
@@ -1,8 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-function jsonHandler<T>(sender: () => Promise<T>) {
+function jsonHandler<T>(sender: (req: VercelRequest) => Promise<T>) {
     return async (req: VercelRequest, res: VercelResponse) => {
-        return res.json(await sender());
+        return res.json(await sender(req));
     }
 }
 
