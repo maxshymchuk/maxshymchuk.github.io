3e077376c13a0bb5142cb55b664b2c8a09fcc580
Max Shymchuk <octorix@mail.ru>
2025-10-04 12:49:46 +0300

    fix (service): change to static import

--- service/src/api/get.data.ts
+++ service/src/api/get.data.ts
@@ -1,8 +1,7 @@
 import { getRepos } from './get.repos';
 import { getUser } from './get.user';
 import { Const } from '../constants';
-
-const contacts = (await import('../data/contacts.json')).default;
+import contacts from '../data/contacts.json';
 
 async function getData(): Promise<UserData> {
     const [user, repositories] = await Promise.all([getUser(), getRepos()]);
