87fa6948f5fdc08f0c0db07e3e5c6665cdae7aaa
Max Shymchuk <octorix@mail.ru>
2026-04-21 03:32:10 +0200

    test: telemetry

--- service/src/functions/data.ts
+++ service/src/functions/data.ts
@@ -1,11 +1,11 @@
 import database from '../redis';
-import { waitUntil } from '@vercel/functions';
+// import { waitUntil } from '@vercel/functions';
 import { INVALIDATE_AFTER_MS } from '../constants';
 import api from '../api';
 import github from '../api/adapters/github';
-import { telemetry } from '../utils';
+// import { telemetry } from '../utils';
 
-export default async function (req: Request) {
+export default async function () {
     const client = api(github);
 
     try {
@@ -23,6 +23,6 @@ export default async function (req: Request) {
 
         return Response.json(data);
     } finally {
-        waitUntil(telemetry(req));
+        // waitUntil(telemetry(req));
     }
 }
