61761ee5ca2e6e101df64540a34efaa4e5634ae3
Max Shymchuk <octorix@mail.ru>
2026-04-21 03:35:57 +0200

    test: add logging

--- service/src/functions/data.ts
+++ service/src/functions/data.ts
@@ -1,19 +1,25 @@
 import database from '../redis';
-// import { waitUntil } from '@vercel/functions';
-import { INVALIDATE_AFTER_MS } from '../constants';
+import { waitUntil } from '@vercel/functions';
+import { INVALIDATE_AFTER_MS, REDIS_URL } from '../constants';
 import api from '../api';
 import github from '../api/adapters/github';
-// import { telemetry } from '../utils';
+import { telemetry } from '../utils';
 
-export default async function () {
+export default async function (req: Request) {
     const client = api(github);
+
+    console.log(REDIS_URL);
 
     try {
         const cache = await database.read<UserData>(database.keys.data);
 
+        console.log('CACHE', cache);
+
         if (cache) return Response.json(cache);
 
         const data = await client.getData();
+
+        console.log('DATA', data);
 
         if (!data) return new Response(null, { status: 404 });
 
@@ -22,7 +28,9 @@ export default async function () {
         });
 
         return Response.json(data);
+    } catch (error) {
+        console.error(error);
     } finally {
-        // waitUntil(telemetry(req));
+        waitUntil(telemetry(req));
     }
 }
